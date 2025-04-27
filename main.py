b37a9f2af971f0aa23ab4ea56db8e3cda5333da9
Nimish Misra <137651308+nimis2708@users.noreply.github.com>
2025-04-26 18:01:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 # Enable CORS for your frontend URL
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://kzmgt0nvxi2tnz4iu7g6.lite.vusercontent.net"],  # Your frontend URL
+    allow_origins=["https://v0-data-science-platform-smoky.vercel.app/"],  # Your frontend URL
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
